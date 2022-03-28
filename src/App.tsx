59f38990f491b9784a9c0d821e69576e84985420
inanocs <nanocabezasanchez@gmail.com>
2022-03-28 22:58:08 +0200

    refactor: move Router to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Footer from "./components/Footer/Footer";
 import MainNav from "./components/MainNav/MainNav";
+import Building from "./pages/Building";
+import CharactersPage from "./pages/CharactersPage";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <MainNav />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <MainNav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/characters" element={<CharactersPage />} />
+          <Route path="/episodes" element={<Building />} />
+          <Route path="/locations" element={<Building />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
